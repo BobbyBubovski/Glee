c90037d5c0ae8cdbee565f28fff3e48890d97292
BobbyBubovski <bobbybubovski@gmail.com>
2025-02-14 01:04:05 +0200

    create product lists

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,34 @@
-$(function(){
+$(function () {
 
     $('.slider__inner').slick({
         dots: true,
-        arrows: false, 
+        arrows: false,
     });
 
     Fancybox.bind("[data-fancybox]", {
 
-      });
+    });
 
-    
+    $('.filter-price__input').ionRangeSlider({
+        type: 'double',
+        prefix: '$',
+        onStart: function (data) {
+            $('.filter-price__from').text(data.from);
+            $('.filter-price__to').text(data.to);
+        },
+        onChange: function (data) {
+            $('.filter-price__from').text(data.from);
+            $('.filter-price__to').text(data.to);
+        },
+    });
+
+    $('.filter-recent__star, .product__star').rateYo({
+        rating: 3.5,
+        starWidth: "11px",
+        normalFill: "#ccccce",
+        ratedFill: "#ffc35b",
+        readOnly: true
+    });
 
 });
+
